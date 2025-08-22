2ebba40bc5457ee5fc34d20793db7a8fbc1e2ec2
zeusato <zeusato@gmail,com>
2025-08-22 16:35:07 +0700

    chore: ignore node_modules & dist; clean gh-pages cache

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/SHS-landing/', // đúng bằng tên repo của đại ca
+  base: '/SHS-Landing/', // đúng bằng tên repo của đại ca
 })
