9ca0ba552b51d915a782eddd6a00eba9ad2d1e7f
zeusato <zeusato@gmail,com>
2025-08-27 18:00:42 +0700

    message

--- src/App.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-import SHSNoScrollLanding from './SHSNoScrollLanding'
-
-export default function App() {
-  return <SHSNoScrollLanding />
-}
--- vite.config.ts
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react()],
-  base: '/SHS-Landing/', // đúng bằng tên repo của đại ca
-})
